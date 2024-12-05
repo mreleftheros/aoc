a83408043eaa9469bb84ce53de7fda59076ec7d4
mreleftheros <eleftheros_all@protonmail.com>
2024-12-05 22:34:11 +0200

    failed to complete part 2

--- src/2024/2.rs
+++ src/2024/2.rs
@@ -10,31 +10,69 @@ fn main() {
                 .split_whitespace()
                 .filter_map(|number_string| number_string.parse().ok())
                 .collect::<Vec<i32>>();
-            are_adjacent_and_ranged(&nums, 1, 3)
+            get_unsafe_indices(&nums, 1, 3).len() == 0
         })
         .count();
     println!("PART 1: {}", safe);
+
+    // part 2
+    let safe = input
+        .lines()
+        .filter(|&l| {
+            let nums = l
+                .split_whitespace()
+                .filter_map(|number_string| number_string.parse().ok())
+                .collect::<Vec<i32>>();
+            let unsafe_indices = get_unsafe_indices(&nums, 1, 3);
+            if unsafe_indices.len() == 0 {
+                true
+            } else {
+                let r = unsafe_indices.iter().fold(false, |mut acc, curr| {
+                    //if acc == true {
+                    // return true;
+                    //}
+                    let nums_filtered = nums
+                        .iter()
+                        .enumerate()
+                        .filter(|(idx, &x)| idx != curr)
+                        .map(|(idx, &x)| x)
+                        .collect::<Vec<_>>();
+                    let u = get_unsafe_indices(&nums_filtered, 1, 3);
+                    if u.len() == 0 {
+                        acc = true;
+                    }
+                    acc
+                });
+                r
+            }
+        })
+        .count();
+    println!("PART 2: {}", safe);
 }
 
-fn are_adjacent_and_ranged(nums: &[i32], min: i32, max: i32) -> bool {
+fn get_unsafe_indices(nums: &[i32], min: i32, max: i32) -> Vec<usize> {
+    let unsafe_indices_vec = Vec::new();
     if nums.len() < 2 {
-        return true;
+        return unsafe_indices_vec;
     }
 
     let is_increasing = nums[1] > nums[0];
 
-    let r = nums.windows(2).fold(true, |acc, curr| {
-        if acc == false {
-            return false;
-        }
-        let is_stepped = if is_increasing {
-            curr[1] > curr[0]
-        } else {
-            curr[0] > curr[1]
-        };
-        let diff = (curr[1] - curr[0]).abs();
-        let is_ranged = diff <= max && diff >= min;
-        is_stepped && is_ranged
-    });
+    let r = nums
+        .windows(2)
+        .enumerate()
+        .fold(unsafe_indices_vec, |mut acc, curr| {
+            let is_stepped = if is_increasing {
+                curr.1[1] > curr.1[0]
+            } else {
+                curr.1[0] > curr.1[1]
+            };
+            let diff = (curr.1[1] - curr.1[0]).abs();
+            let is_ranged = diff <= max && diff >= min;
+            if !is_stepped || !is_ranged {
+                acc.push(curr.0 + 1);
+            }
+            acc
+        });
     r
 }
